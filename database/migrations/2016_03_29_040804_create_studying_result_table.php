467ce09de592fb0a7330c6268d89451d40524e9e
Katathip <earth2933370@gmail.com>
2016-03-29 11:23:44 +0700

    add Migration

---
+++ database/migrations/2016_03_29_040804_create_studying_result_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudyingResultTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studying_result', function (Blueprint $table) {
+            $table->bigIncrements('sr_id');
+            $table->bigIncrements('user_id');
+            $table->bigIncrements('course_enroll_id');
+            $table->bigIncrements('user_id');
+            $table->float('score',3,2)->nullable();
+            $table->timestamps('update_timestamp');
+            $table->timestamps('last_update');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('studying_result');
+    }
+}
