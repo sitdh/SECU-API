3584c090592607c2455e94c7b9e15c98f779f80f
Kitti Klinbua <codetotti@gmail.com>
2016-04-01 13:58:49 +0700

    update AssignmentMeta Model

--- app/AssignmentMeta.php
+++ app/AssignmentMeta.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AssignmentMeta extends Model {
 
-	protected $fillable = [];
+	protected $fillable = ['assignment_id','assignment_meta_key','assignment_metavalue'];
 
 	protected $dates = [];
 
